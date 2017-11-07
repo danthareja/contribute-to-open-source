8616cb6862f41f300497aa95139fcd30d076a388
Dan Thareja <danthareja+github@gmail.com>
2017-11-07 14:07:25 -0700

    Fix style

--- src/calculator.js
+++ src/calculator.js
@@ -3,30 +3,30 @@ exports._check = (...args) => {
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
-  args.forEach(arg => {
+  args.forEach((arg) => {
     if (typeof arg !== 'number') {
-      throw new TypeError(`${arg} is not a number`)
+      throw new TypeError(`${arg} is not a number`);
     }
   });
 };
 
 exports.add = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x / y;
 };
 
