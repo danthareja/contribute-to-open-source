260ff0dd77438a18a19ab96a471889809bc2a67a
Sourabh <sourabhpruthi10@gmail.com>
2018-01-14 19:08:57 +0530

    Review change

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,4 @@
-exports._check = () => {
-  let x;
-  let y;
+exports._check = (x,y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
@@ -14,22 +12,22 @@ exports._check = () => {
 };
 
 exports.add = (x, y) => {
-  exports._check();
+  exports._check(x,y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check();
+  exports._check(x,y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check();
+  exports._check(x,y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check();
+  exports._check(x,y);
   return x / y;
 };
 
