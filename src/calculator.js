da11348f9b68e99d0535ffddfd8e27cce1c37ddf
Aditya Sahu <adithebest15@gmail.com>
2018-02-10 00:37:28 +0530

    Implement_check

--- src/calculator.js
+++ src/calculator.js
@@ -4,7 +4,14 @@ exports._check = () => {
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
 };
-
+exports._check = (x, y) => { 
+   if (typeof x !== 'number') { 
+     throw new TypeError(`${x} is not a number`); 
+   } 
+   if (typeof y !== 'number') { 
+      throw new TypeError(`${y} is not a number`); 
+   } 
+  } 
 exports.add = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
@@ -12,6 +19,7 @@ exports.add = (x, y) => {
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
+  exports._check(x, y); 
   return x + y;
 };
 
@@ -22,6 +30,7 @@ exports.subtract = (x, y) => {
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
+  exports._check(x, y); 
   return x - y;
 };
 
@@ -32,6 +41,7 @@ exports.multiply = (x, y) => {
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
+  exports._check(x, y); 
   return x * y;
 };
 
@@ -42,6 +52,7 @@ exports.divide = (x, y) => {
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
+  exports._check(x, y); 
   return x / y;
 };
 
