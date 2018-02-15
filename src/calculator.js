8c100d33e16736bbbe74055bd7ced7c9c35a20a0
MattyPlo <matthew.ploszaj@seattlecentral.edu>
2018-02-14 18:48:26 -0800

    style guide changes made

--- src/calculator.js
+++ src/calculator.js
@@ -10,29 +10,20 @@ exports._check = (x, y) => {
     throw new TypeError(`${y} is not a number`);
   }
 };
-
 exports.add = (x, y) => {
-
-  exports._check(x, y)
+  exports._check(x, y);
   return x + y;
 };
-
 exports.subtract = (x, y) => {
-  
-  exports._check(x, y)
+  exports._check(x, y);
   return x - y;
 };
-
 exports.multiply = (x, y) => {
-  
-  exports._check(x, y)
+  exports._check(x, y);
   return x * y;
 };
-
 exports.divide = (x, y) => {
-  
-  exports._check(x, y)
+  exports._check(x, y);
   return x / y;
 };
-
 module.exports = exports;
