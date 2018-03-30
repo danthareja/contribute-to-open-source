27c718dcc13869b6bc8a811f8f47643429e5a9b4
Carlo De Sutter <carlodesutter@gmail.com>
2018-03-30 14:30:52 +0200

    Implement _check

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
+describe('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
-  });
-
+  });  
+  
   afterEach(() => {
     calculator._check.restore();
   });
