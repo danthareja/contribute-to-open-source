55e15a8f67464e1f7ef3e29c0259dc2974e11364
tspakes <tspakes@vols.utk.edu>
2018-04-02 16:54:38 -0400

    Add back required spaces after if's

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,8 @@
 exports._check = (a, b) => {
-  if(typeof a !== 'number') {
+  if (typeof a !== 'number') {
     throw new TypeError(`${a} is not a number`);
   }
-  if(typeof b !== 'number') {
+  if (typeof b !== 'number') {
     throw new TypeError(`${b} is not a number`);
   }
 };
