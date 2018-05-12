7522aeb9bc15060d8ae55bdf7debe4ba9e500466
Winfield Ly <winfield.ly@protonmail.com>
2018-05-12 16:19:43 -0400

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,10 @@
 exports._check = (x, y) => {
-   if (typeof x !== 'number') {
-     throw new TypeError(`${x} is not a number`);
-   }
-   if (typeof y !== 'number') {
-     throw new TypeError(`${y} is not a number`);
-   }
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
 };
 
 exports.add = (x, y) => {
