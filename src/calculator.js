d2a16a071d30397dad1108c30e0093f6c7385636
kleyu <jackob.naskreski@gmail.com>
2018-06-09 01:30:25 +0200

    Fix _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,8 @@
 exports._check = (x, y) => {
+  // DRY up the codebase with this function
+  // First, move the duplicate error checking code here
+  // Then, invoke this function inside each of the others
+  // HINT: you can invoke this function with exports._check()
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
