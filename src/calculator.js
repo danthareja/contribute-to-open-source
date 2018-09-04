35f63264b28682609c8aa1cf8644c0eaff9ee328
Ghost <itsomarhamdan@gmail.com>
2018-09-03 21:21:47 -0500

    implement _check() with styles edited

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,4 @@
-exports._check = (x,y) => {
+exports._check = (x, y) => {
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
@@ -13,24 +13,31 @@ exports._check = (x,y) => {
 };
 
 exports.add = (x, y) => {
-  if(exports._check(x,y)==true){
-  return x + y;}
-
+  if (exports._check(x, y) === true) {
+    return x + y;
+  }
+  return 0;
 };
 
 exports.subtract = (x, y) => {
-  if(exports._check(x,y)==true){
-  return x - y;}
+  if (exports._check(x, y) === true) {
+    return x - y;
+  }
+  return 0;
 };
 
 exports.multiply = (x, y) => {
-if(exports._check(x,y)==true){
-  return x * y;}
+  if (exports._check(x, y) === true) {
+    return x * y;
+  }
+  return 0;
 };
 
 exports.divide = (x, y) => {
-  if(exports._check(x,y)==true){
-  return x / y;}
+  if (exports._check(x, y) === true) {
+    return x / y;
+  }
+  return 0;
 };
 
 module.exports = exports;
