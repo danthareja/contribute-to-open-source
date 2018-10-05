7757f97cce422854c1d56df6fd7d2ea0545717f3
Jessica <jessicalim@bell.net>
2018-10-04 22:47:26 -0400

    Cleaned up dry code

--- src/calculator.js
+++ src/calculator.js
@@ -5,42 +5,19 @@ exports._check = () => {
   // HINT: you can invoke this function with exports._check()
 };
 
-exports.add = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+exports.add_check = (x, y) => {
   return x + y;
 };
 
-exports.subtract = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+exports.subtract_check = (x, y) => {
   return x - y;
 };
 
-exports.multiply = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+exports.multiply_check = (x, y) => {
   return x * y;
 };
 
-exports.divide = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
+exports.divide_check = (x, y) => {
   }
   return x / y;
 };
