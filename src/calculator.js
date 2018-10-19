c2ebd03bae4023a36d8d9855c27a189330ec01b7
Cam Utz <Dragon8029@gmail.com>
2018-10-18 21:17:36 -0700

    fixed styling problems

--- src/calculator.js
+++ src/calculator.js
@@ -6,26 +6,25 @@
       throw new TypeError(`${y} is not a number`);
     }
   };
-  
+
   exports.add = (x, y) => {
     exports._check(x, y);
     return x + y;
   };
 
+  exports.subtract = (x, y) => {
+    exports._check(x, y);
+    return x - y;
+  };
 
-exports.subtract = (x, y) => {
-  exports._check(x, y);
-  return x - y;
-};
+  exports.multiply = (x, y) => {
+    exports._check(x, y);
+    return x * y;
+  };
 
-exports.multiply = (x, y) => {
-  exports._check(x, y);
-  return x * y;
-};
+  exports.divide = (x, y) => {
+    exports._check(x, y);
+    return x / y;
+  };
 
-exports.divide = (x, y) => {
-  exports._check(x, y);
-  return x / y;
-};
-
-module.exports = exports;
+  module.exports = exports;
