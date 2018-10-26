130b388634898b2ea475dbeab86b7a0a2d697ae2
root <rgonzalez7760@gmail.com>
2018-10-26 11:49:35 -0500

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,9 @@
-exports._check = () => {
-  // DRY up the codebase with this function
-  // First, move the duplicate error checking code here
-  // Then, invoke this function inside each of the others
+ exports._check = () => {
+   // DRY up the codebase with this function
+   // First, move the duplicate error checking code here
+   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
 };
-
 exports.add = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
@@ -12,9 +11,9 @@ exports.add = (x, y) => {
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
+exports.add_check = (x, y) => {
   return x + y;
 };
-
 exports.subtract = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
@@ -22,9 +21,9 @@ exports.subtract = (x, y) => {
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
+exports.subtract_check = (x, y) => {
   return x - y;
 };
-
 exports.multiply = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
@@ -32,17 +31,18 @@ exports.multiply = (x, y) => {
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
+exports.multiply_check = (x, y) => {
   return x * y;
 };
-
 exports.divide = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
+exports.divide_check = (x, y) => {
   }
   return x / y;
 };
-
+ 
 module.exports = exports;
