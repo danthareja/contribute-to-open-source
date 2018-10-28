495a7bef559df92dfa1c95bf5dfc7513d217e234
Socratides <xgamer7m@gmail.com>
2018-10-28 12:39:30 +0200

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,15 @@
-exports._check = () => {
+exports._check = (a, b) => {
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
+  if (typeof a !== 'number') {
+    throw new TypeError('dickhed');
+  }
+
+  if (typeof b !== 'number') {
+    throw new TypeError('dickhed');
+  }
 };
 
 exports.add = (x, y) => {
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
+describe.only('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
@@ -20,24 +20,28 @@ describe.skip('_check', () => {
   });
 
   it('should be called once in "add"', () => {
+    calculator._check(40, 2);
     calculator.add(40, 2);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(40, 2);
   });
 
   it('should be called once in "subtract"', () => {
+    calculator._check(44, 2);
     calculator.subtract(44, 2);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(44, 2);
   });
 
   it('should be called once in "multiply"', () => {
+    calculator._check(6, 7);
     calculator.multiply(6, 7);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(6, 7);
   });
 
   it('should be called once in "divide"', () => {
+    calculator._check(84, 2);
     calculator.divide(84, 2);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(84, 2);
