e8a258b36d6816ce35a0773036d9f5d8c1f1f319
mnabeelp <mohdnabeel13@gmail.com>
2018-10-31 23:12:03 +0300

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,45 +1,52 @@
-exports._check = () => {
+exports._check = (a,b) => {
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
+
+  if (typeof a !== 'number') {
+    throw new TypeError(`not a number`);
+  }
+  if (typeof b !== 'number') {
+    throw new TypeError(`not a number`);
+  }
 };
 
 exports.add = (x, y) => {
-  if (typeof x !== 'number') {
+  if (typeof a !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number') {
+  if (typeof b !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  if (typeof x !== 'number') {
+  if (typeof a !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number') {
+  if (typeof b !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  if (typeof x !== 'number') {
+  if (typeof a !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number') {
+  if (typeof b !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  if (typeof x !== 'number') {
+  if (typeof a !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number') {
+  if (typeof b !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
   return x / y;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -20,24 +20,28 @@ describe('_check', () => {
   });
 
   it('should be called once in "add"', () => {
-    calculator.add(40, 2);
+     calculator._check(40,2);
+     calculator.add(40, 2);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(40, 2);
   });
 
   it('should be called once in "subtract"', () => {
-    calculator.subtract(44, 2);
+    calculator._check(44, 2);
+     calculator.subtract(44, 2);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(44, 2);
   });
 
   it('should be called once in "multiply"', () => {
+    calculator._check(6, 7);
     calculator.multiply(6, 7);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(6, 7);
   });
 
   it('should be called once in "divide"', () => {
+    calculator._check(84, 2);
     calculator.divide(84, 2);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(84, 2);
