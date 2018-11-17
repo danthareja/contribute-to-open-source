8bd4694190e351474bb2775675b6f9e421134b68
Sai Kiran Goud <saikiran.goud@kony.com>
2018-11-17 16:34:01 +0530

    Implemented _check() and fixed lint errors

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,5 @@
-exports._check = (x,y) => {
- if (typeof x !== 'number') {
+exports._check = (x, y) => {
+  if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
@@ -8,7 +8,7 @@ exports._check = (x,y) => {
 };
 
 exports.add = (x, y) => {
- exports._check(x, y);
+  exports._check(x, y);
   return x + y;
 };
 
@@ -23,7 +23,7 @@ exports.multiply = (x, y) => {
 };
 
 exports.divide = (x, y) => {
-   exports._check(x, y);
+  exports._check(x, y);
   return x / y;
 };
 
