c5ece85b6e7016d0f822b88aa7830bf648037ec7
Aman Jain <aman.jain01@sap.com>
2018-12-12 10:55:17 +0530

    Implement_Check

--- src/calculator.js
+++ src/calculator.js
@@ -4,13 +4,12 @@ exports._check = (x, y) => {
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
   if (typeof x !== 'number') {
-      throw new TypeError(`${x} is not a number`);
+    throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
-      throw new TypeError(`${y} is not a number`);
+    throw new TypeError(`${y} is not a number`);
   }
-};
-      
+};      
 exports.add = (x, y) => {
   exports._check(x, y);
   return x + y;
