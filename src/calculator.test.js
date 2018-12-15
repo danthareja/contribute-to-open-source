65708e0ffc3677c87eddc3293c03fc27fe90dbc6
unknown <chris.a.desilva@gmail.com>
2018-12-15 17:19:20 -0600

    ran npm lint, updated file to follow style guidelines

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe('_check', (x, y) => {
+describe('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
