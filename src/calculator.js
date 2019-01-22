7f352f704c2c3b8f9e373ea669b233e745b79fbb
Max Esser <maxesser32@icloud.com>
2019-01-21 23:56:13 -0800

    Fixed some Formatting issues

--- src/calculator.js
+++ src/calculator.js
@@ -1,27 +1,27 @@
 // This is my contribute
 exports._check = (x, y) => {
-    if (typeof x !== 'number') {
-        throw new TypeError('$(x) is not numer');
-    }
-    if (typeof y !== 'number') {
-        throw new TypeError('$(y) is not a number');
-    }
-    return x + y;
+  if (typeof x !== 'number') {
+    throw new TypeError('$(x) is not numer');
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError('$(y) is not a number');
+  }
+  return x + y;
 };
 exports.add = (x, y) => {
-    exports._check(x, y);
-    return x + y;
+  exports._check(x, y);
+  return x + y;
 };
 exports.subtract = (x, y) => {
-    exports._check(x, y);
-    return x - y;
+  exports._check(x, y);
+  return x - y;
 };
 exports.multiply = (x, y) => {
-    exports._check(x, y);
-    return x * y;
+  exports._check(x, y);
+  return x * y;
 };
 exports.divide = (x, y) => {
-    exports._check(x, y);
-    return x / y;
+  exports._check(x, y);
+  return x / y;
 };
 module.exports = exports;
