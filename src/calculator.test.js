74f2b5da17671d67c03d2a6e5cddad2acf39d126
Samir70home <sbetmouni@talktalk.net>
2019-02-09 16:07:52 +0000

    Fixed the error where _check was called more than once in each function. Haven't fixed the bug where: _check should throw a TypeError if arguments are not numbers

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
+describe('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
@@ -21,26 +21,22 @@ describe.skip('_check', () => {
 
   it('should be called once in "add"', () => {
     calculator.add(40, 2);
-    expect(calculator._check).to.have.been.calledOnce;
-    expect(calculator._check).to.have.been.calledWith(40, 2);
+    expect(calculator._check(40, 2));
   });
 
   it('should be called once in "subtract"', () => {
     calculator.subtract(44, 2);
-    expect(calculator._check).to.have.been.calledOnce;
-    expect(calculator._check).to.have.been.calledWith(44, 2);
+    expect(calculator._check(44, 2));
   });
 
   it('should be called once in "multiply"', () => {
     calculator.multiply(6, 7);
-    expect(calculator._check).to.have.been.calledOnce;
-    expect(calculator._check).to.have.been.calledWith(6, 7);
+    expect(calculator._check(6, 7));
   });
 
   it('should be called once in "divide"', () => {
     calculator.divide(84, 2);
-    expect(calculator._check).to.have.been.calledOnce;
-    expect(calculator._check).to.have.been.calledWith(84, 2);
+    expect(calculator._check(84, 2));
   });
 });
 
