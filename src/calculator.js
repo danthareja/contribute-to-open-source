bccb57c8b83e0bafac63982f40b02a8f1591fd9b
JohnStorgion <johnStorgion@gmail.com>
2019-04-26 14:16:05 -0500

    added functionality and unit tests for calculating square roots, natural logs, and log base

--- src/calculator.js
+++ src/calculator.js
@@ -42,7 +42,45 @@ exports.divide = (x, y) => {
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
+  if(y == 0) {
+    throw new TypeError("Can't divide by zero!");
+  }
   return x / y;
 };
 
+exports.squareRoot = (x) => {
+  if (typeof x !== 'number') {
+    throw new TypeError('${x} is not a number');
+  }
+  if(x < 0) {
+    throw new TypeError("Can't take square root of a negative number!");
+  }
+  return Math.sqrt(x);
+}
+
+exports.log10 = (x) => {
+  if(typeof x !== 'number') {
+    throw new TypeError('%{x} is not a number');
+  }
+  if(x <= 0) {
+    throw new TypeError("Log can't accept negative or 0 input");
+  }
+  if(x === 1) {
+    throw new TypeError("Log can't accept input of 1");
+  }
+  return Math.log10(x);
+}
+
+exports.ln = (x) => {
+  if(typeof x !== 'number') {
+    throw new TypeError('%{x} is not a number');
+  }
+  if(x <= 0) {
+    throw new TypeError("ln can't accept negative or 0 input");
+  }
+  if(x === 1) {
+    throw new TypeError("ln can't accept input of 1");
+  }
+  return Math.log(x);
+}
 module.exports = exports;
