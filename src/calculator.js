bc67996b7c62528560c07e78570c1d4559227750
Tiina Malinen <malitsu@gmail.com>
2019-05-21 20:17:52 +0300

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -3,6 +3,48 @@ exports._check = () => {
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
+  describe('_check', () => {
+    beforeEach(() => {
+      sinon.spy(calculator, '_check');
+    });
+  
+    afterEach(() => {
+      calculator._check.restore();
+    });
+  
+    it('should throw a TypeError if arguments are not numbers', () => {
+      expect(() => calculator._check(40, '2')).to.throw(TypeError);
+      expect(() => calculator._check(40, [])).to.throw(TypeError);
+      expect(() => calculator._check(40, {})).to.throw(TypeError);
+      expect(() => calculator._check('40', 2)).to.throw(TypeError);
+      expect(() => calculator._check([], 2)).to.throw(TypeError);
+      expect(() => calculator._check({}, 2)).to.throw(TypeError);
+    });
+  
+    it('should be called once in "add"', () => {
+      calculator.add(40, 2);
+      expect(calculator._check).to.have.been.calledOnce;
+      expect(calculator._check).to.have.been.calledWith(40, 2);
+    });
+  
+    it('should be called once in "subtract"', () => {
+      calculator.subtract(44, 2);
+      expect(calculator._check).to.have.been.calledOnce;
+      expect(calculator._check).to.have.been.calledWith(44, 2);
+    });
+  
+    it('should be called once in "multiply"', () => {
+      calculator.multiply(6, 7);
+      expect(calculator._check).to.have.been.calledOnce;
+      expect(calculator._check).to.have.been.calledWith(6, 7);
+    });
+  
+    it('should be called once in "divide"', () => {
+      calculator.divide(84, 2);
+      expect(calculator._check).to.have.been.calledOnce;
+      expect(calculator._check).to.have.been.calledWith(84, 2);
+    });
+  });
 };
 
 exports.add = (x, y) => {
@@ -13,6 +55,7 @@ exports.add = (x, y) => {
     throw new TypeError(`${y} is not a number`);
   }
   return x + y;
+  exports._check()
 };
 
 exports.subtract = (x, y) => {
@@ -23,6 +66,7 @@ exports.subtract = (x, y) => {
     throw new TypeError(`${y} is not a number`);
   }
   return x - y;
+  exports._check()
 };
 
 exports.multiply = (x, y) => {
@@ -33,6 +77,7 @@ exports.multiply = (x, y) => {
     throw new TypeError(`${y} is not a number`);
   }
   return x * y;
+  exports._check()
 };
 
 exports.divide = (x, y) => {
@@ -43,6 +88,7 @@ exports.divide = (x, y) => {
     throw new TypeError(`${y} is not a number`);
   }
   return x / y;
+  exports._check()
 };
 
 module.exports = exports;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,49 +1,6 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
-  beforeEach(() => {
-    sinon.spy(calculator, '_check');
-  });
-
-  afterEach(() => {
-    calculator._check.restore();
-  });
-
-  it('should throw a TypeError if arguments are not numbers', () => {
-    expect(() => calculator._check(40, '2')).to.throw(TypeError);
-    expect(() => calculator._check(40, [])).to.throw(TypeError);
-    expect(() => calculator._check(40, {})).to.throw(TypeError);
-    expect(() => calculator._check('40', 2)).to.throw(TypeError);
-    expect(() => calculator._check([], 2)).to.throw(TypeError);
-    expect(() => calculator._check({}, 2)).to.throw(TypeError);
-  });
-
-  it('should be called once in "add"', () => {
-    calculator.add(40, 2);
-    expect(calculator._check).to.have.been.calledOnce;
-    expect(calculator._check).to.have.been.calledWith(40, 2);
-  });
-
-  it('should be called once in "subtract"', () => {
-    calculator.subtract(44, 2);
-    expect(calculator._check).to.have.been.calledOnce;
-    expect(calculator._check).to.have.been.calledWith(44, 2);
-  });
-
-  it('should be called once in "multiply"', () => {
-    calculator.multiply(6, 7);
-    expect(calculator._check).to.have.been.calledOnce;
-    expect(calculator._check).to.have.been.calledWith(6, 7);
-  });
-
-  it('should be called once in "divide"', () => {
-    calculator.divide(84, 2);
-    expect(calculator._check).to.have.been.calledOnce;
-    expect(calculator._check).to.have.been.calledWith(84, 2);
-  });
-});
-
 describe('add', () => {
   it('should throw a TypeError if arguments are not numbers', () => {
     expect(() => calculator.add(40, '2')).to.throw(TypeError);
