6d4d29c1859ce75f34ee0fa7bd52c40e1470bd9c
Alexander Schelchere <ascellie@gmail.com>
2019-06-16 18:04:03 -0400

    ran npm run lint -- --fix

--- src/calculator.js
+++ src/calculator.js
@@ -8,22 +8,22 @@ exports._check = (x, y) => {
 };
 
 exports.add = (x, y) => {
-  exports._check(x,y)
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x,y)
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x,y)
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x,y)
+  exports._check(x, y);
   return x / y;
 };
 
