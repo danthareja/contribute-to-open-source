a11bee1c4e50f041b6e27125a560c300a42096c3
esotericbob <esotericbob@hotmail.com>
2019-08-12 22:36:10 +0100

    Implement _check

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -20,7 +20,7 @@ describe('_check', () => {
   });
 
   it('should be called once in "add"', () => {
-    calculator.add(40, 2);
+    calculator.add(40,2);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(40, 2);
   });
