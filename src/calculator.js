7fd5e68080b58cc00cdb16ce5d5de968706c0a08
jsonsinger <hello@jasonsinger.co.za>
2019-10-13 15:00:15 +0200

    Fixed style guide violations

--- src/calculator.js
+++ src/calculator.js
@@ -1,30 +1,28 @@
-exports._check = (x,y) => {
+export function _check(x, y) {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
-};
+}
 
-exports.add = (x, y) => {
-  exports._check(x,y);
+export function add(x, y) {
+  _check(x, y);
   return x + y;
-};
+}
 
-exports.subtract = (x, y) => {
-  exports._check(x,y);
+export function subtract(x, y) {
+  _check(x, y);
   return x - y;
-};
+}
 
-exports.multiply = (x, y) => {
-  exports._check(x,y);
+export function multiply(x, y) {
+  _check(x, y);
   return x * y;
-};
+}
 
-exports.divide = (x, y) => {
-  exports._check(x,y);
+export function divide(x, y) {
+  _check(x, y);
   return x / y;
-};
-
-module.exports = exports;
+}
