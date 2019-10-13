48fe0df0ee57816170375725a3282d3b180debb1
jsonsinger <hello@jasonsinger.co.za>
2019-10-13 15:03:37 +0200

    Undo update to ES6 module export.

--- src/calculator.js
+++ src/calculator.js
@@ -1,28 +1,31 @@
-export function _check(x, y) {
+exports._check = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
+
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
-}
+};
 
-export function add(x, y) {
-  _check(x, y);
+exports.add = (x, y) => {
+  exports._check(x, y);
   return x + y;
-}
+};
 
-export function subtract(x, y) {
-  _check(x, y);
+exports.subtract = (x, y) => {
+  exports._check(x, y);
   return x - y;
-}
+};
 
-export function multiply(x, y) {
-  _check(x, y);
+exports.multiply = (x, y) => {
+  exports._check(x, y);
   return x * y;
-}
+};
 
-export function divide(x, y) {
-  _check(x, y);
+exports.divide = (x, y) => {
+  exports._check(x, y);
   return x / y;
-}
+};
+
+module.exports = exports;
