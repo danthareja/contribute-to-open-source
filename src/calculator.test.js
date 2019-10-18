e43777e05bd109fae118b62f8b3bbc5546807e94
JuWeber99 <s_weberj@stud.hwr-berlin.de>
2019-10-18 10:46:14 +0200

    test passing - first contribution :)

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -20,26 +20,26 @@ describe('_check', () => {
 
   it('should be called once in "add"', () => {
     calculator.add(40, 2);
-    expect((calculator._check).have.been.calledOnce);
-    expect(calculator._check).have.been.calledWith(40, 2);
+    expect((calculator._check).calledOnce);
+    expect(calculator._check).calledWith(40, 2);
   });
 
   it('should be called once in "subtract"', () => {
     calculator.subtract(44, 2);
-    expect((calculator._check).have.been.calledOnce);
-    expect(calculator._check).have.been.calledWith(44, 2);
+    expect((calculator._check).calledOnce);
+    expect(calculator._check).calledWith(44, 2);
   });
 
   it('should be called once in "multiply"', () => {
     calculator.multiply(6, 7);
-    expect((calculator._check).have.been.calledOnce);
-    expect(calculator._check).have.been.calledWith(6, 7);
+    expect((calculator._check).calledOnce);
+    expect(calculator._check).calledWith(6, 7);
   });
 
   it('should be called once in "divide"', () => {
     calculator.divide(84, 2);
-    expect((calculator._check).have.been.calledOnce);
-    expect(calculator._check).have.been.calledWith(84, 2);
+    expect((calculator._check).calledOnce);
+    expect(calculator._check).calledWith(84, 2);
   });
 });
 
