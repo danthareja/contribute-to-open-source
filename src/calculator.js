de3c8cdaba1a9bebf3a920bdba1b86d0c473d4e6
SurajKhayamali <35559169+SurajKhayamali@users.noreply.github.com>
2019-10-18 15:01:05 +0545

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,10 @@
-exports._check = () => {
+exports._check = (x, y) => {
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
