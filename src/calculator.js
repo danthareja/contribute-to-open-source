fe831c341b1966252bf0906dd4b8260583eca02b
burnout87 <gabriele.barni@gmail.com>
2019-11-12 21:05:34 +0100

    again commit

--- src/calculator.js
+++ src/calculator.js
@@ -2,6 +2,7 @@ exports._check = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
+  
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
