f1b28e02837f08ae60b07c0a2f6c745c40f3da72
NikitaMasand <masandnikita@gmail.com>
2020-01-09 16:31:22 +0530

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,18 +1,14 @@
 exports._check = (x, y) => {
   // DRY up the codebase with this function
-  //DRY, it's Do Not Repeat Yourself
-  //Initially we were doing the same error checking again and again for all the operations
-  //Our mission now is to make sure all that error checking is done once here.
-  
   // First, move the duplicate error checking code here
+  // Then, invoke this function inside each of the others
+  // HINT: you can invoke this function with exports._check()
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
-  // Then, invoke this function inside each of the others
-  // HINT: you can invoke this function with exports._check()
 };
 
 exports.add = (x, y) => {
