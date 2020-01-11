c1fd064c7f10cc00efd1dfd83fa69c7f0ddd7860
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-01-12 00:49:41 +0300

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,55 +1,36 @@
-exports._check = (a,b) => {
-  // DRY up the codebase with this function
-  // First, move the duplicate error checking code here
-  // Then, invoke this function inside each of the others
-  // HINT: you can invoke this function with exports._check()
+exports._check = (a, b) => {
+    // DRY up the codebase with this function
+    // First, move the duplicate error checking code here
+    // Then, invoke this function inside each of the others
+    // HINT: you can invoke this function with exports._check()
 
-  if (typeof a !== 'number') {
-    throw new TypeError(`not a number`);
-  }
-  if (typeof b !== 'number') {
-    throw new TypeError(`not a number`);
-  }
+    if (typeof a !== `number`) {
+        throw new TypeError('not a number');
+    }
+    if (typeof b !== `number`) {
+        throw new TypeError('not a number');
+    }
 };
 
 exports.add = (x, y) => {
-  if (typeof a !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof b !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
-  return x + y;
+    //exports._check(x, y);
+
+    return x + y;
 };
 
 exports.subtract = (x, y) => {
-  if (typeof a !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof b !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
-  return x - y;
+    //exports._check(x, y);
+    return x - y;
 };
 
 exports.multiply = (x, y) => {
-  if (typeof a !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof b !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
-  return x * y;
+    //exports._check(x, y);
+    return x * y;
 };
 
 exports.divide = (x, y) => {
-  if (typeof a !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof b !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
-  return x / y;
+    //exports._check(x, y);
+    return x / y;
 };
 
 module.exports = exports;
