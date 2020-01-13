394b18e00f05845960cc7f0c5f6e0a5f0e80c76b
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-01-13 11:25:15 +0300

    Modified code in calculator.js and calculator.test.js to avoid repetitions.

--- src/calculator.js
+++ src/calculator.js
@@ -13,24 +13,18 @@ exports._check = (a, b) => {
 };
 
 exports.add = (x, y) => {
-    //exports._check(x, y);
-
     return x + y;
-};
-
+}
 exports.subtract = (x, y) => {
-    //exports._check(x, y);
     return x - y;
-};
+}
 
 exports.multiply = (x, y) => {
-    //exports._check(x, y);
     return x * y;
-};
+}
 
 exports.divide = (x, y) => {
-    //exports._check(x, y);
     return x / y;
-};
+}
 
 module.exports = exports;
