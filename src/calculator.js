a219af6dff050d5adf5370d2c29590e85da0a93e
Will Barnard <willbarnard687@pm.me>
2020-02-13 15:04:52 +0000

    Update calculator.js
    Updated by running npm lint, forgot to use correct indentation

--- src/calculator.js
+++ src/calculator.js
@@ -18,18 +18,18 @@ exports.add = (x, y) => {
   return x + y;
 };
 exports.subtract = (x, y) => {
-exports._check(x, y);
-return x - y;
+  exports._check(x, y);
+  return x - y;
 };
 
 exports.multiply = (x, y) => {
-exports._check(x, y);
-return x * y;
+  exports._check(x, y);
+  return x * y;
 };
 
 exports.divide = (x, y) => {
-exports._check(x, y);
-return x / y;
+  exports._check(x, y);
+  return x / y;
 };
 
 module.exports = exports;
