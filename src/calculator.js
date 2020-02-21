fe1c4308168d74e9b88e46a9d6201b0a6f84c46e
MJW19 <mw00@gmx.com>
2020-02-21 13:54:42 +0000

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -6,43 +6,53 @@ exports._check = () => {
 };
 
 exports.add = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
-  return x + y;
-};
+  exports._check = (x, y) => {
+    if (typeof x !== 'number') {
+      throw new TypeError(`${x} is not a number`);
+    }
+    if (typeof y !== 'number') {
+      throw new TypeError(`${y} is not a number`);
+    }
+    return x + y;
+  };
 
-exports.subtract = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
-  return x - y;
-};
+  exports.add = (x, y) => {
+    exports._check(x, y);
+    return x + y;
+  };
 
-exports.multiply = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
-  return x * y;
-};
+  exports.subtract = (x, y) => {
+    if (typeof x !== 'number') {
+      throw new TypeError(`${x} is not a number`);
+    }
+    if (typeof y !== 'number') {
+      throw new TypeError(`${y} is not a number`);
+    }
+    exports._check(x, y);
+    return x - y;
+  };
 
-exports.divide = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
-  return x / y;
-};
+  exports.multiply = (x, y) => {
+    if (typeof x !== 'number') {
+      throw new TypeError(`${x} is not a number`);
+    }
+    if (typeof y !== 'number') {
+      throw new TypeError(`${y} is not a number`);
+    }
+    exports._check(x, y);
+    return x * y;
+  };
 
-module.exports = exports;
+  exports.divide = (x, y) => {
+    if (typeof x !== 'number') {
+      throw new TypeError(`${x} is not a number`);
+    }
+    if (typeof y !== 'number') {
+      throw new TypeError(`${y} is not a number`);
+    }
+    exports._check(x, y);
+    return x / y;
+  };
+
+  module.exports = exports;
+};
