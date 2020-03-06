685371dc2ea9a9ed7a87b45e55fcafd5d0bae299
Malik <malikmukhtar63@gmail.com>
2020-03-06 01:39:45 +0100

    fixed indentation and changed describe.skip to describe

--- src/calculator.js
+++ src/calculator.js
@@ -9,12 +9,12 @@ exports._check = (x, y) => {
 
 exports.add = (x, y) => {
   exports._check(x, y);
-    return x + y;
+  return x + y;
 };
 
 exports.subtract = (x, y) => {
   exports._check(x, y);
-   return x - y;
+  return x - y;
 };
 
 exports.multiply = (x, y) => {
