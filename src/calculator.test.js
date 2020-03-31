38c75774ed7f676d47c3dc5a716bcab984763927
Semaj Primm <semajthomasprimm@gmail.com>
2020-03-30 22:29:34 -0400

    Implement _check

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.only('_check', () => {
+describe('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
@@ -13,7 +13,7 @@ describe.only('_check', () => {
   it('should throw a TypeError if arguments are not numbers', () => {
     expect(() => calculator._check(40, '2')).to.throw(TypeError);
     expect(() => calculator._check(40, [])).to.throw(TypeError);
-::    expect(() => calculator._check(40, {})).to.throw(TypeError);
+    expect(() => calculator._check(40, {})).to.throw(TypeError);
     expect(() => calculator._check('40', 2)).to.throw(TypeError);
     expect(() => calculator._check([], 2)).to.throw(TypeError);
     expect(() => calculator._check({}, 2)).to.throw(TypeError);
