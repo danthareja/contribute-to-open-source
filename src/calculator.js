1c354c3a59f87c9f0b79f8af6c7ffb881084b848
patthrasher <patrickalanthrasher@gmail.com>
2020-04-22 01:06:48 -0500

    changed style guide violations

--- src/calculator.js
+++ src/calculator.js
@@ -4,30 +4,30 @@ exports._check = (x, y) => {
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
   if (typeof x !== 'number') {
-    throw new TypeError(`xit is not a number`);
+    throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
-    throw new TypeError(`yit is not a number`);
+    throw new TypeError(`${y} is not a number`);
   }
 };
 
 exports.add = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x / y;
 };
 
