b012f4ba6a2fc11aa5de659c55d504bc7037c204
harshu1470 <harshit1470@gmail.com>
2020-05-14 19:57:09 +0530

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -4,10 +4,10 @@ exports._check = (x, y) => {
 
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
-  }
+}
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
-  }
+}
 
 
   // Then, invoke this function inside each of the others
@@ -17,21 +17,21 @@ exports._check = (x, y) => {
 exports.add = (x, y) => {
    exports._check(x, y);
    return x + y;
-};
+}
 
 exports.subtract = (x, y) => {
    exports._check(x, y);
    return x - y;
-};
+}
 
 exports.multiply = (x, y) => {
    exports._check(x, y);
    return x * y;
-};
+}
 
 exports.divide = (x, y) => {
    exports._check(x, y);
    return x / y;
-};
+}
 
 module.exports = exports;
