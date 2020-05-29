b95533a1c6e82e70e15bf33154cf7e0dc8f867f4
tiagostutz <tiagostutz@gmail.com>
2020-05-29 10:00:46 -0300

    refs #1. changing `this.` from `exports.`, as suggested in the comments

--- src/calculator.js
+++ src/calculator.js
@@ -12,22 +12,22 @@ exports._check = (x, y) => {
 };
 
 exports.add = (x, y) => {
-  this._check(x, y);
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  this._check(x, y);
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  this._check(x, y);
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  this._check(x, y);
+  exports._check(x, y);
   return x / y;
 };
 
