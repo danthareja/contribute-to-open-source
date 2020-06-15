ac04c40c6978ac1c6d19cdb5e92195279e8f9ae7
mjbh831 <56654912+mjbh831@users.noreply.github.com>
2020-06-15 16:15:01 -0500

    fixed double quote styling error

--- src/calculator.js
+++ src/calculator.js
@@ -1,29 +1,49 @@
-exports._check = (x, y) => {
-  if (typeof x !== "number") {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== "number") {
-    throw new TypeError(`${y} is not a number`);
-  }
+exports._check = () => {
+  // if (typeof x !== "number") {
+  //   throw new TypeError(`${x} is not a number`);
+  // }
+  // if (typeof y !== "number") {
+  //   throw new TypeError(`${y} is not a number`);
+  // }
 };
 
 exports.add = (x, y) => {
-  exports._check(x, y);
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x, y);
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x, y);
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x, y);
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
   return x / y;
 };
 
