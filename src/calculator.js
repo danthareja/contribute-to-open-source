abfc2cf98a7f10ece682d72b55c90b15bc4dd31a
Gustavo Oliveira <gtanansi@gmail.com>
2020-06-18 13:58:09 -0300

    Change PR

--- src/calculator.js
+++ src/calculator.js
@@ -1,37 +1,33 @@
-exports._check = (x,y) => {
+exports._check = (x, y) => {
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
-if (typeof x !== 'number') {
-  throw new TypeError(`${x} is not a number`);
-}
-if (typeof y !== 'number') {
-  throw new TypeError(`${y} is not a number`);
-}
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
 };
 
 exports.add = (x, y) => {
-  
-  exports._check(x,y)
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-
-  exports._check(x,y)
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  
-  exports._check(x,y)
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  
-  exports._check(x,y)
+  exports._check(x, y);
   return x / y;
 };
 
