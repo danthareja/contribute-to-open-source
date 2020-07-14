a45ffc73b92049fb6626ee07c2ec52ddb2cdf9c6
jane muthoni <muchirijane@outlook.com>
2020-07-14 20:43:34 +0300

    run the tests

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,4 @@
-exports._check = (x,y) => {
+exports._check = (x, y) => {
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
@@ -31,7 +31,7 @@ exports.multiply = (x, y) => {
 
 exports.divide = (x, y) => {
   exports._check(x, y);
-  
+
   return x / y;
 };
 
