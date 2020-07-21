2c58ea3595ee66012ec76e680b6ef9c7f2fa7fc4
Ramon Ferreira do Nascimento <ramon.nascimento@labbs.com.br>
2020-07-21 20:23:50 -0300

    adjust eslint sintax error

--- src/calculator.js
+++ src/calculator.js
@@ -13,25 +13,21 @@ exports._check = (x, y) => {
 
 exports.add = (x, y) => {
   exports._check(x, y);
-
   return x + y;
 };
 
 exports.subtract = (x, y) => {
   exports._check(x, y);
-
   return x - y;
 };
 
 exports.multiply = (x, y) => {
   exports._check(x, y);
-
   return x * y;
 };
 
 exports.divide = (x, y) => {
   exports._check(x, y);
-  
   return x / y;
 };
 
