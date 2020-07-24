4d943aeaca07b2fdd02b0f5a76ae79ad2ff878ea
Andreas Wagner <andreas.wagner@mni.thm.de>
2020-07-24 17:46:35 +0200

    format code so it matches the lint guideline

--- src/calculator.js
+++ src/calculator.js
@@ -1,30 +1,29 @@
-exports._check = (x,y) => {
-  if (typeof x !== 'number'){
+exports._check = (x, y) => {
+  if (typeof x !== "number") {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number'){
+  if (typeof y !== "number") {
     throw new TypeError(`${y} is not a number`);
   }
-  
 };
 
 exports.add = (x, y) => {
-  this._check(x,y);
+  this._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  this._check(x,y);
+  this._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  this._check(x,y);
+  this._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  this._check(x,y);
+  this._check(x, y);
   return x / y;
 };
 
