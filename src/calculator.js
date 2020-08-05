36f65567e7b26be771e927769c4aea5ca820ba19
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-08-05 15:33:23 +0700

    Indentation and comma fix

--- src/calculator.js
+++ src/calculator.js
@@ -1,13 +1,13 @@
 exports._check = (x, y) => {
-    if (typeof x !== 'number'){
-        throw new TypeError(`${x} is not a number`);
-    }
-    if (typeof y !== 'number'){
-        throw new TypeError(`${y} is not a number`);
-    }
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
 }
 exports.add = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x + y;
 };
 
