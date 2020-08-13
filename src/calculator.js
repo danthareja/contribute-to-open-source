ad34b0106bf7451ad786e22e2b046204a6b3d9f7
Felipe Augusto <felipe.santana@bb.com.br>
2020-08-13 12:03:11 -0300

    fix

--- src/calculator.js
+++ src/calculator.js
@@ -9,27 +9,26 @@ exports._check = (x, y) => {
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
-
-}
+};
 
 exports.add = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x + y;
-}
+};
 
 exports.subtract = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x - y;
-}
+};
 
 exports.multiply = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x * y;
-}
+};
 
 exports.divide = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x / y;
-}
+};
 
 module.exports = exports;
