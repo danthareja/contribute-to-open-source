d8e3fc0ede9fb2b065fe5ab06340bca8c2984caa
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-09-02 10:51:16 +0100

    Implement style guidelines

--- src/calculator.js
+++ src/calculator.js
@@ -6,40 +6,40 @@ exports._check = () => {
 };
 
 exports.add = (x, y) => {
-  if (typeof x !== "number") {
+  if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== "number") {
+  if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  if (typeof x !== "number") {
+  if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== "number") {
+  if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  if (typeof x !== "number") {
+  if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== "number") {
+  if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  if (typeof x !== "number") {
+  if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== "number") {
+  if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
   return x / y;
