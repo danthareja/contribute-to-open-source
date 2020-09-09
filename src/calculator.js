d37ebd818f261b85d91736add4e526b12ccd335d
chirag agrawal <agrawalchirag035@gmail.com>
2020-09-09 16:52:04 +0530

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -19,7 +19,7 @@ exports.add = (x, y) => {
   // if (typeof y !== 'number') {
   //   throw new TypeError(`${y} is not a number`);
   // }
-  exports._check(x,y)
+  exports._check(x, y);
   return x + y;
 };
 
@@ -30,7 +30,7 @@ exports.subtract = (x, y) => {
   // if (typeof y !== 'number') {
   //   throw new TypeError(`${y} is not a number`);
   // }
-  exports._check(x,y)
+  exports._check(x, y);
   return x - y;
 };
 
@@ -41,7 +41,7 @@ exports.multiply = (x, y) => {
   // if (typeof y !== 'number') {
   //   throw new TypeError(`${y} is not a number`);
   // }
-  exports._check(x,y)
+  exports._check(x, y);
   return x * y;
 };
 
@@ -52,7 +52,7 @@ exports.divide = (x, y) => {
   // if (typeof y !== 'number') {
   //   throw new TypeError(`${y} is not a number`);
   // }
-  // exports._check(x,y)
+  exports._check(x, y);
   return x / y;
 };
 
