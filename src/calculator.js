2f8b10c86e76da997aed6eb93f77552818564068
Kaanhan Okyay <kaanhanokyay@gmail.com>
2021-01-14 22:48:25 -0500

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -5,29 +5,25 @@ exports._check = (x, y) => {
   }
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
-  }	
+  }
 };
 
 exports.add = (x, y) => {
-
   exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-
   exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-
   exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-
   exports._check(x, y);
   return x / y;
 };
