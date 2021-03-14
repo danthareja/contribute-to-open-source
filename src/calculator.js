e033363db694d634f505996dc2836ed2cedcaada
Aadit <baggaaadit017@gmail.com>
2021-03-14 18:32:31 +0530

    updating calculator.js
    wrote a function for recurring statements

--- src/calculator.js
+++ src/calculator.js
@@ -6,43 +6,30 @@ exports._check = () => {
 };
 
 exports.add = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+checkThem(x,y)
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  checkThem(x,y)
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  checkThem(x,y)
   return x * y;
 };
 
 exports.divide = (x, y) => {
+  checkThem(x,y)
+  return x / y;
+};
+function checkThem(x,y){
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
-  return x / y;
-};
-
+} 
 module.exports = exports;
