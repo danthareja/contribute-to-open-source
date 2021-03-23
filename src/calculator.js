e330b186fbaf7e447149cdf8fc7b49a06aa90224
prajwal.bk <prajwal.bk@metricstream.com>
2021-03-24 01:12:08 +0530

    Correcting the styling errors

--- src/calculator.js
+++ src/calculator.js
@@ -1,30 +1,30 @@
 exports._check = (x, y) => {
-	if (typeof x !== 'number') {
-		throw new TypeError(`${x} is not a number`);
-	}
-	if (typeof y !== 'number') {
-		throw new TypeError(`${y} is not a number`);
-	}
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
 };
 
 exports.add = (x, y) => {
-	exports._check(x, y);
-	return x + y;
+  exports._check(x, y);
+  return x + y;
 };
 
 exports.subtract = (x, y) => {
-	exports._check(x, y);
-	return x - y;
+  exports._check(x, y);
+  return x - y;
 };
 
 exports.multiply = (x, y) => {
-	exports._check(x, y);
-	return x * y;
+  exports._check(x, y);
+  return x * y;
 };
 
 exports.divide = (x, y) => {
-	exports._check(x, y);
-	return x / y;
+  exports._check(x, y);
+  return x / y;
 };
 
 module.exports = exports;
