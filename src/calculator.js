cbea93e549ac3f66e2e6f8099b5a714328b651e2
Keya Sengupta <keyasengupta@Keyas-MacBook-Pro.local>
2021-04-15 12:25:21 -0400

    Lint fix

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,5 @@
 exports._check = (x, y) => {
- if (typeof x !== 'number') {
+  if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
@@ -23,7 +23,7 @@ exports.multiply = (x, y) => {
 };
 
 exports.divide = (x, y) => {
-   exports._check(x, y);
+  exports._check(x, y);
   return x / y;
 };
 
