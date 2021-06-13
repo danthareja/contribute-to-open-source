91a4ed14888a3c218af095326c7cbf4245a5112e
ddoyediran <ddoyediran@gmail.com>
2021-06-13 18:26:32 +0200

    DRY up the codebase

--- src/calculator.js
+++ src/calculator.js
@@ -1,47 +1,45 @@
-exports._check = () => {
+//exports._check = () => {
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
-};
+//};
 
-exports.add = (x, y) => {
-  if (typeof x !== 'number') {
+exports._check = () => {
+  // check if x !== number
+  if(typeof x !== 'number') {
+    // throw error x NAN
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number') {
+
+  // check if y !== number
+  if(typeof y !== 'number') {
+    // throw error y NAN
     throw new TypeError(`${y} is not a number`);
-  }
+  }  
+}
+
+exports.add = (x, y) => {
+ // call exports function
+ exports._check(x,y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  // call exports function
+  exports._check(x,y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  // call exports function
+  exports._check(x,y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  // call exports function
+  exports._check(x,y);
   return x / y;
 };
 
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
-  beforeEach(() => {
-    sinon.spy(calculator, '_check');
-  });
+// describe.skip('_check', () => {
+//   beforeEach(() => {
+//     sinon.spy(calculator, '_check');
+//   });
+
+describe('_check', () => {
+    beforeEach(() => {
+      sinon.spy(calculator, '_check');
+    })
 
   afterEach(() => {
     calculator._check.restore();
