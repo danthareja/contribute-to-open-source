16330ca6fd6618c62b081ea4e2d3eac27e3a62b2
Sanket2055 <sanketdiwate95@gmail.com>
2021-09-06 17:30:33 +0530

    improved  code2

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,4 @@
-exports._check = (x , y) => {
+exports._check = (x, y) => {
 
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
@@ -7,21 +7,21 @@ exports._check = (x , y) => {
     throw new TypeError(`${y} is not a number`);
   }
 };
-exports.add = (x , y) => {
-  exports._check(x , y);
+exports.add = (x, y) => {
+  exports._check(x, y);
   return x + y;
 };
-exports.subtract = (x , y) => {
-  exports._check(x , y);
+exports.subtract = (x, y) => {
+  exports._check(x, y);
   return x - y;
 };
 
-exports.multiply = (x , y) => {
-  exports._check(x , y);
+exports.multiply = (x, y) => {
+  exports._check(x, y);
   return x * y;
 };
-exports.divide = (x , y) => {
-  exports._check(x , y);
+exports.divide = (x, y) => {
+  exports._check(x, y);
   return x / y;
 };
 module.exports = exports;
