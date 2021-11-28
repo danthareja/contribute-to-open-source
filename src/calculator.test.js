b57e793aa93547165e90229f710423aae4d29313
Prasanna Paithankar <82210129+PrasannaPaithankar@users.noreply.github.com>
2021-11-28 16:11:36 +0530

    Update calculator.test.js

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
+describe.omly('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
