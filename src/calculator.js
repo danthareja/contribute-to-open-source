795a9bdd1b6912902b8a9ab507284319d6db01e1
Ankur Upadhyay <ankurup3@gmail.com>
2022-01-04 03:22:33 +0530

    Added changes

--- src/calculator.js
+++ src/calculator.js
@@ -1,31 +1,31 @@
-exports._check = (x,y) => {
-  if(typeof x!== 'number') {
+exports._check = (x, y) => {
+  if (typeof x !== 'number') {
     throw new TypeError('${x} is not a number');
   }
 
-  if(typeof y!== 'number') {
+  if (typeof y !== 'number') {
     throw new TypeError('${y} is not a number');
   }
-  return x+y;
+  return x + y;
 };
 
 exports.add = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x / y;
 };
 
