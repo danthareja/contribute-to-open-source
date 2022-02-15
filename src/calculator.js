4078ed2fb80822254ee794879b411ff7640412dc
harshi1122 <harshitha1155@gmail.com>
2022-02-15 22:00:53 +0530

    'Changes'

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,8 @@
-exports._check = (x, y) => { 
+exports._check = (x, y) => {
+  // DRY up the codebase with this function
+  // First, move the duplicate error checking code here
+  // Then, invoke this function inside each of the others
+  // HINT: you can invoke this function with exports._check()
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
@@ -12,7 +16,7 @@ exports.add = (x, y) => {
   return x + y;
 };
 
-exports.subtract = (x, y) => { 
+exports.subtract = (x, y) => {
   exports._check(x, y);
   return x - y;
 };
