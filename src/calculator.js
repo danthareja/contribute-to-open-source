5ac922f5abc4488a0be08ef3d8d152c068d9ad7e
ArpanInstabase <arpan.balpande@instabase.com>
2022-06-05 14:08:19 +0530

    style changes

--- src/calculator.js
+++ src/calculator.js
@@ -3,12 +3,12 @@ exports._check = (x, y) => {
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
-  if (typeof x !== "number") {
-		throw new TypeError(`${x} is not a number`)
-	}
-	if (typeof y !== "number") {
-		throw new TypeError(`${y} is not a number`)
-	}
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
 };
 
 exports.add = (x, y) => {
