4d383e3a3c287cd447d1aa19d75d59797edc96a6
arsal uddin <arsaluddin134@gmail.com>
2022-07-24 20:06:38 +0530

    just started contributing😀

--- src/calculator.js
+++ src/calculator.js
@@ -15,33 +15,33 @@ exports.add = (x, y) => {
 };
 
 exports.subtract = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
+  if (typeof x == 'number' && typeof y == 'number') {
+    return x - y;
   }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
+  else{
+    throw new TypeError(`input is not a number`);
   }
-  return x - y;
+  
 };
 
 exports.multiply = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
+  if (typeof x == 'number' && typeof y == 'number') {
+      return x * y;
   }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
+   else {
+    throw new TypeError(`input is not a number`);
   }
-  return x * y;
+
 };
 
 exports.divide = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
+  if (typeof x == 'number' && typeof y == 'number') {
+    return x / y;
   }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
+  else {
+    throw new TypeError(`input is not a number`);
   }
-  return x / y;
+
 };
 
 module.exports = exports;
