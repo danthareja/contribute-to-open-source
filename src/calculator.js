034bf5f90047808bf3aaa5631febbce35c0bde37
DKendle <dina.kendle@gmail.com>
2022-10-03 11:41:17 -0400

    Implement _check Second Try

--- src/calculator.js
+++ src/calculator.js
@@ -12,22 +12,22 @@ exports._check = (x, y) => {
 };
 
 exports.add = (x, y) => {
-  exports._check(x ,y)
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x ,y)
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x ,y)
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x ,y)
+  exports._check(x, y);
   return x / y;
 };
 
