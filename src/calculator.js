cde49c45768433048ec789b80ab59db97402689a
Connie Park <conniedc1206@gmail.com>
2022-10-04 11:30:24 -0700

    Fixed code's styling using npm run lint

--- src/calculator.js
+++ src/calculator.js
@@ -3,23 +3,23 @@ exports._check = (x, y) => {
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
-  exports.add = (x, y) => {
-    exports._check(x, y)
+  exports.add = () => {
+    exports._check(x, y);
     return x + y;
   };
-  
-  exports.subtract = (x, y) => {
-    exports._check(x, y)
+
+  exports.subtract = () => {
+    exports._check(x, y);
     return x - y;
   };
-  
-  exports.multiply = (x, y) => {
-    exports._check(x, y)
+
+  exports.multiply = () => {
+    exports._check(x, y);
     return x * y;
   };
-  
-  exports.divide = (x, y) => {
-    exports._check(x, y)
+
+  exports.divide = () => {
+    exports._check(x, y);
     return x / y;
   };
 
