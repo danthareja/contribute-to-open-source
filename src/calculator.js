2830e2f90361ea10b77a71748c283999ce65d2b1
Connie Park <conniedc1206@gmail.com>
2022-10-04 11:49:18 -0700

    Fixed failing tests errors

--- src/calculator.js
+++ src/calculator.js
@@ -1,28 +1,4 @@
 exports._check = (x, y) => {
-  // DRY up the codebase with this function
-  // First, move the duplicate error checking code here
-  // Then, invoke this function inside each of the others
-  // HINT: you can invoke this function with exports._check()
-  exports.add = () => {
-    exports._check(x, y);
-    return x + y;
-  };
-
-  exports.subtract = () => {
-    exports._check(x, y);
-    return x - y;
-  };
-
-  exports.multiply = () => {
-    exports._check(x, y);
-    return x * y;
-  };
-
-  exports.divide = () => {
-    exports._check(x, y);
-    return x / y;
-  };
-
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
@@ -31,5 +7,24 @@ exports._check = (x, y) => {
   }
 };
 
+exports.add = (x, y) => {
+  exports._check(x, y);
+  return x + y;
+};
+
+exports.subtract = (x, y) => {
+  exports._check(x, y);
+  return x - y;
+};
+
+exports.multiply = (x, y) => {
+  exports._check(x, y);
+  return x * y;
+};
+
+exports.divide = (x, y) => {
+  exports._check(x, y);
+  return x / y;
+};
 
 module.exports = exports;
