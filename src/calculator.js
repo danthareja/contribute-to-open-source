5deeb3236065aea6ba4018ada19bf23d8284fdd8
Lagstill <kpdlakshmi1970@gmail.com>
2022-10-08 12:14:22 +0530

    Fixed DRY up the codebase #1

--- src/calculator.js
+++ src/calculator.js
@@ -1,11 +1,19 @@
-exports._check = () => {
+exports._check = (x,y) => {
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
+
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
-  // HINT: you can invoke this function with exports._check()
+  // HINT: you can invoke this function with exports._check(x,y)
 };
 
 exports.add = (x, y) => {
+  exports._check(x,y)
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
@@ -16,6 +24,7 @@ exports.add = (x, y) => {
 };
 
 exports.subtract = (x, y) => {
+  exports._check(x,y)
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
@@ -26,6 +35,7 @@ exports.subtract = (x, y) => {
 };
 
 exports.multiply = (x, y) => {
+  exports._check(x,y)
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
@@ -36,6 +46,7 @@ exports.multiply = (x, y) => {
 };
 
 exports.divide = (x, y) => {
+  exports._check(x,y)
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
