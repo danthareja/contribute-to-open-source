747df8808e9cc1c668a0f806c9d46747c90beb8e
Mithil Joshi <mithil_joshi@persistent.com>
2022-10-10 14:42:07 +0530

    rectified linting errors

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,5 @@
 exports._check = (x, y) => {
-if (typeof x !== 'number') {
+  if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
@@ -8,22 +8,22 @@ if (typeof x !== 'number') {
 };
 
 exports.add = (x, y) => {
- exports._check(x, y);
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
- exports._check(x, y);
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
- exports._check(x, y);
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
- exports._check(x, y);
+  exports._check(x, y);
   return x / y;
 };
 
