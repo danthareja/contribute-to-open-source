710cde2c4e8d224b2ccb0d18022098f50d653886
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-10-12 08:58:10 +0300

    code linting

--- src/calculator.js
+++ src/calculator.js
@@ -1,26 +1,26 @@
 exports._check = (x, y) => {
-  num = 'number'
-  if (typeof x !== num) throw new TypeError(`${x} is not a ${num}`)
-  if (typeof y !== num) throw new TypeError(`${y} is not a ${num}`)
+  let num = 'number';
+  if (typeof x !== num) throw new TypeError(`${x} is not a ${num}`);
+  if (typeof y !== num) throw new TypeError(`${y} is not a ${num}`);
 };
 
 exports.add = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x / y;
 };
 
