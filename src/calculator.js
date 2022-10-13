158a1040f264c7054886bf200554ba72913b5115
frosty <ashh.gould@gmail.com>
2022-10-13 09:37:40 +0100

    implement _check
    Remove duplicate typeerrors

--- src/calculator.js
+++ src/calculator.js
@@ -9,50 +9,26 @@ exports._check = (x, y) => {
 };
 
 exports.add = (x, y) => {
-  if (this._check(x, y)) {
-    if (typeof x !== 'number') {
-      throw new TypeError(`${x} is not a number`);
-    }
-    if (typeof y !== 'number') {
-      throw new TypeError(`${y} is not a number`);
-    }
+  if (this._check(x, y)) {   
     return x + y;
   }
 };
   
 
 exports.subtract = (x, y) => {
-  if (this._check(x, y)) {
-    if (typeof x !== 'number') {
-      throw new TypeError(`${x} is not a number`);
-    }
-    if (typeof y !== 'number') {
-      throw new TypeError(`${y} is not a number`);
-    }
+  if (this._check(x, y)) {   
     return x - y;
   }
 };
 
 exports.multiply = (x, y) => {
-  if (this._check(x, y)) {
-    if (typeof x !== 'number') {
-      throw new TypeError(`${x} is not a number`);
-    }
-    if (typeof y !== 'number') {
-      throw new TypeError(`${y} is not a number`);
-    }
+  if (this._check(x, y)) {   
     return x * y;
   }
 };
 
 exports.divide = (x, y) => {
-  if (this._check(x, y)) {
-    if (typeof x !== 'number') {
-      throw new TypeError(`${x} is not a number`);
-    }
-    if (typeof y !== 'number') {
-      throw new TypeError(`${y} is not a number`);
-    }
+  if (this._check(x, y)) {   
     return x / y;
   }
 };
