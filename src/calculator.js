40900ed30984352e2922c7c029375ff68f03609d
Juanma Romero <pro@juanmaroni.me>
2022-11-04 16:14:43 +0100

    Code style fix

--- src/calculator.js
+++ src/calculator.js
@@ -6,6 +6,7 @@ exports._check = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
+  
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
@@ -19,7 +20,7 @@ exports.add = (x, y) => {
 
 exports.subtract = (x, y) => {
   exports._check(x, y);
-  
+
   return x - y;
 };
 
@@ -31,7 +32,7 @@ exports.multiply = (x, y) => {
 
 exports.divide = (x, y) => {
   exports._check(x, y);
-  
+
   return x / y;
 };
 
