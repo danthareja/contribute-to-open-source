13e6767f4e354fd09c16b9cc224a91a760970004
osama <osamasiddiquee007@gmail.com>
2022-12-28 22:48:52 +0530

    modified code

--- src/calculator.js
+++ src/calculator.js
@@ -1,35 +1,35 @@
-exports._check = (x,y) => {
+function typeCheck(num) {
+  if (typeof num !== 'number') {
+    throw new TypeError(`${num} is not a number`);
+  }
+}
+
+exports._check = (x, y) => {
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
   typeCheck(x);
-  typeCheck(y);  
+  typeCheck(y);
 };
 
-function typeCheck(num){
-	if(typeof num !== 'number'){
-		throw new TypeError(`${num} is not a number`);
-	}
-}
-
 exports.add = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x,y); 
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x,y);  
+  exports._check(x, y);
   return x / y;
 };
 
