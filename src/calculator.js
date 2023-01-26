8efe1a7ffb2edfa418459293f1cb54ee204eca49
will513 <will513r@gmail.com>
2023-01-26 15:32:14 -0500

    cleaned up code

--- src/calculator.js
+++ src/calculator.js
@@ -1,33 +1,36 @@
-exports._check = (x, y) => {
+const checkNumber = (x, y) => {
   if (typeof x !== "number") {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== "number") {
     throw new TypeError(`${y} is not a number`);
   }
-
 };
 
-exports.add = (x, y) => {
-  exports._check(x, y);
+const checkDivideByZero = (y) => {
+  if (y === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+};
+
+exports.add = (x, y = 0) => {
+  checkNumber(x, y);
   return x + y;
 };
 
-exports.subtract = (x, y) => {
-  exports._check(x, y);
+exports.subtract = (x, y = 0) => {
+  checkNumber(x, y);
   return x - y;
 };
 
-exports.multiply = (x, y) => {
-  exports._check(x, y);
+exports.multiply = (x, y = 1) => {
+  checkNumber(x, y);
   return x * y;
 };
 
-exports.divide = (x, y) => {
-  exports._check(x, y);
-   if (y === 0) {
-     throw new Error("Cannot divide by zero");
-   }
+exports.divide = (x, y = 1) => {
+  checkNumber(x, y);
+  checkDivideByZero(y);
   return x / y;
 };
 
