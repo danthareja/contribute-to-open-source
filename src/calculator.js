d464794bd253f774fb2f8c5e1ffbc103da47b7b5
Tom <55595095+tmjpt@users.noreply.github.com>
2023-02-06 10:58:25 +0000

    Removed comments

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,4 @@
 exports._check = (...args) => {
-  // DRY up the codebase with this function
-  // First, move the duplicate error checking code here
-  // Then, invoke this function inside each of the others
-  // HINT: you can invoke this function with exports._check()
   args.forEach((arg) => {
     if (typeof arg !== 'number') {
       throw new TypeError(`${arg} is not a number`);
