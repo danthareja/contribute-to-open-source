530d5b672adec2e342c1b6eb1b247d43aa690f3f
Joseph Francioso <joefrancioso@gmail.com>
2023-03-23 23:16:20 -0400

    Submitting fixes to lint styling guide

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,10 @@
 exports._check = (x, y) => {
-    if (typeof x !== 'number') {
-        throw new TypeError(`${x} is not a number`);
-    }
-    if (typeof y !== 'number') {
-        throw new TypeError(`${y} is not a number`);
-    }
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
 
     // DRY up the codebase with this function
     // First, move the duplicate error checking code here
@@ -13,23 +13,23 @@ exports._check = (x, y) => {
 };
 
 exports.add = (x, y) => {
-    exports._check(x, y);
-    return x + y;
+  exports._check(x, y);
+  return x + y;
 };
 
 exports.subtract = (x, y) => {
-    exports._check(x, y);
-    return x - y;
+  exports._check(x, y);
+  return x - y;
 };
 
 exports.multiply = (x, y) => {
-    exports._check(x, y);
-    return x * y;
+  exports._check(x, y);
+  return x * y;
 };
 
 exports.divide = (x, y) => {
-    exports._check(x, y);
-    return x / y;
+  exports._check(x, y);
+  return x / y;
 };
 
 module.exports = exports;
