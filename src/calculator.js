b34f1e4557c8514883ee117a2ba1fa628be0f6e6
Joey F <joefrancioso@gmail.com>
2023-03-23 22:16:41 -0400

    Fixed duplication check code block
    The code has a duplication problem where the type check code block is repeated in every function. The comment in the code suggests DRY (Don't Repeat Yourself) principles should be applied by creating a shared function to check the type of the arguments.

--- src/calculator.js
+++ src/calculator.js
@@ -5,44 +5,34 @@ exports._check = () => {
   // HINT: you can invoke this function with exports._check()
 };
 
-exports.add = (x, y) => {
+// This function checks if the given arguments are of type `number`
+// and throws a TypeError if not.
+const _check = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
     throw new TypeError(`${y} is not a number`);
   }
+};
+
+exports.add = (x, y) => {
+  _check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  _check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  _check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  _check(x, y);
   return x / y;
 };
 
-module.exports = exports;
