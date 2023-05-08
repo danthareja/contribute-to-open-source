c44ac9cbe7bbe4adcdeb96b18dce1aef58f08e7d
nikhilagastya <karthikagastya2002@gmail.com>
2023-05-08 21:57:20 +0530

    changed expressions

--- src/calculator.js
+++ src/calculator.js
@@ -8,22 +8,22 @@ exports._check = (x, y) => {
 };
 
 exports.add = (x, y) => {
-  this._check(x, y);
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  this._check(x, y);
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  this._check(x, y);
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  this._check(x, y);
+  exports._check(x, y);
   return x / y;
 };
 
