5a761c4429adde1341f881ae3d927d44667fa6c1
Kwabena Sarkodieh <104778334+KwabenaSark@users.noreply.github.com>
2023-06-25 19:48:50 +0100

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,7 @@ exports._check = () => {
   // DRY up the codebase with this function
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
-  // HINT: you can invoke this function with exports._check() 
+  // HINT: you can invoke this function with exports._check() .
 };
 
 exports.add = (x, y) => {
