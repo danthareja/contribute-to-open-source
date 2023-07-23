fe59bdf831c9136b90523d5c0b084e72d3c4b526
Steven Espi <espisteven5@gmail.com>
2023-07-23 10:57:21 -0700

    Update calculator.js
    Updated space formatting

--- src/calculator.js
+++ src/calculator.js
@@ -1,25 +1,25 @@
 exports._check = (x, y) => {
-    if (typeof x !== 'number' || typeof y !== 'number') {
-        throw new TypeError('Both inputs must be numbers');
+  if (typeof x !== 'number' || typeof y !== 'number') {
+    throw new TypeError('Both inputs must be numbers');
     }
 };
 
 exports.add = (x, y) => {
-    exports._check(x, y); // Validate inputs using _check function
+  exports._check(x, y); // Validate inputs using _check function
     return x + y;
 };
 
 exports.subtract = (x, y) => {
-    exports._check(x, y); // Validate inputs using _check function
+  exports._check(x, y); // Validate inputs using _check function
     return x - y;
 };
 
 exports.multiply = (x, y) => {
-    exports._check(x, y); // Validate inputs using _check function
+  exports._check(x, y); // Validate inputs using _check function
     return x * y;
 };
 
 exports.divide = (x, y) => {
-    exports._check(x, y); // Validate inputs using _check function
+  exports._check(x, y); // Validate inputs using _check function
     return x / y;
 };
