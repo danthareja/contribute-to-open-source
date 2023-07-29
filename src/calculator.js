655d2e35d7cc83252eccb4cb28e921310f35842d
Monique <monique.parnis@gmail.com>
2023-07-29 17:16:07 +0100

    Fix style guide violations

--- src/calculator.js
+++ src/calculator.js
@@ -13,18 +13,24 @@ exports.add = (x, y) => {
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x / y;
 };
 
 module.exports = exports;
+
+
+// DRY up the codebase with this function
+  // First, move the duplicate error checking code here
+  // Then, invoke this function inside each of the others
+  // HINT: you can invoke this function with exports._check()
