c174af5843647e872df02c441f85b54d7ee540b3
Godwin Nduka <136750604+gnduka@users.noreply.github.com>
2023-09-01 21:13:01 +0100

    Improvements made upon pull request review.

--- src/calculator.js
+++ src/calculator.js
@@ -4,34 +4,26 @@ exports._check = (x, y) => {
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
 
-   if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+     if (typeof x !== 'number') {
+      throw new TypeError(`${x} is not a number`);
+    }
+    if (typeof y !== 'number') {
+      throw new TypeError(`${y} is not a number`);
+    }
 };
-
 exports.add = (x, y) => {
-
   exports._check(x, y);
   return x + y;
 };
-
 exports.subtract = (x, y) => {
-
   exports._check(x, y);
   return x - y;
 };
-
 exports.multiply = (x, y) => {
-
   exports._check(x, y);
   return x * y;
 };
-
 exports.divide = (x, y) => {
-
   exports._check(x, y);
   return x / y;
 };
