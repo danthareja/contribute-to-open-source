0f7db4c78f4b468e537f61015f4b6fd9209cc234
Leonardo Rossi <>
2023-10-30 15:45:54 +0100

    Style fixes

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,10 @@
 exports._check = (val1, val2) => {
   if (typeof val1 !== 'number') {
     throw new TypeError(`${val1} is not a number`);
-  }  
+  }
   if (typeof val2 !== 'number') {
     throw new TypeError(`${val2} is not a number`);
-  }  
+  }
 };
 
 exports.add = (x, y) => {
