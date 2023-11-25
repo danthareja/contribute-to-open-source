d618742aeaf682e1812cca420b6c64796106e473
bacemouedrni <bacemouedrni@gmail.com>
2023-11-25 23:46:35 +0200

    make check function and invoke it , make tests

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.only('_check', () => {
+describe('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
