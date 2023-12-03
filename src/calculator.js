18c9397b112490d5fddb7be9f9643ebcfa22b123
abidnk <abidnk34@gmail.com>
2023-12-03 13:26:32 +0530

    Added the error checking logic within the _check function and utilizes it within each arithmetic function

--- src/calculator.js
+++ src/calculator.js
@@ -5,6 +5,15 @@ exports._check = () => {
   // HINT: you can invoke this function with exports._check()
 };
 
+exports._check = (x, y) => {
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
+};
+
 exports.add = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
