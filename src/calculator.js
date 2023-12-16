8cab433d9ca04aa73c48b48fe2486085319d7afe
yash <ys77357@gmail.com>
2023-12-16 22:27:21 +0530

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,5 @@
-exports._check = (x,y) => {
-  if(typeof x!== 'number' || typeof y !== 'number'){
+exports._check = (x, y) => {
+  if (typeof x !== "number" || typeof y !== "number") {
     throw new TypeError(`${x} is not a number`);
   }
   // DRY up the codebase with this function
@@ -9,44 +9,44 @@ exports._check = (x,y) => {
 };
 
 exports.add = (x, y) => {
-  exports._check(40,2);
-  if (typeof x !== 'number') {
+  exports._check(40, 2);
+  if (typeof x !== "number") {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number') {
+  if (typeof y !== "number") {
     throw new TypeError(`${y} is not a number`);
   }
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(44,2);
-  if (typeof x !== 'number') {
+  exports._check(44, 2);
+  if (typeof x !== "number") {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number') {
+  if (typeof y !== "number") {
     throw new TypeError(`${y} is not a number`);
   }
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(6,7);
-  if (typeof x !== 'number') {
+  exports._check(6, 7);
+  if (typeof x !== "number") {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number') {
+  if (typeof y !== "number") {
     throw new TypeError(`${y} is not a number`);
   }
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(84,2);
-  if (typeof x !== 'number') {
+  exports._check(84, 2);
+  if (typeof x !== "number") {
     throw new TypeError(`${x} is not a number`);
   }
-  if (typeof y !== 'number') {
+  if (typeof y !== "number") {
     throw new TypeError(`${y} is not a number`);
   }
   return x / y;
