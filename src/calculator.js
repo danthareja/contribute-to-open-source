79504c014030ebb35f1af3908917b294ad5e53fa
frank-suwen <suwenw2@outlook.com>
2023-12-20 23:44:03 -0800

    fix style issues

--- src/calculator.js
+++ src/calculator.js
@@ -12,49 +12,25 @@ exports._check = (x, y) => {
 };
 
 exports.add = (x, y) => {
-  // if (typeof x !== 'number') {
-  //   throw new TypeError(`${x} is not a number`);
-  // }
-  // if (typeof y !== 'number') {
-  //   throw new TypeError(`${y} is not a number`);
-  // }
-  exports._check(x, y)
+  exports._check(x, y);
 
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  // if (typeof x !== 'number') {
-  //   throw new TypeError(`${x} is not a number`);
-  // }
-  // if (typeof y !== 'number') {
-  //   throw new TypeError(`${y} is not a number`);
-  // }
-  exports._check(x, y)
+  exports._check(x, y);
 
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  // if (typeof x !== 'number') {
-  //   throw new TypeError(`${x} is not a number`);
-  // }
-  // if (typeof y !== 'number') {
-  //   throw new TypeError(`${y} is not a number`);
-  // }
-  exports._check(x, y)
+  exports._check(x, y);
 
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  // if (typeof x !== 'number') {
-  //   throw new TypeError(`${x} is not a number`);
-  // }
-  // if (typeof y !== 'number') {
-  //   throw new TypeError(`${y} is not a number`);
-  // }
-  exports._check(x, y)
+  exports._check(x, y);
 
   return x / y;
 };
