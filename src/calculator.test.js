1ffc793aaa1fbd0811efdda47af8fb0bbd1f31fd
xyj140707 <xyj140707@gmail.com>
2024-03-16 10:17:31 -0400

    Implement _check

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,8 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
+describe('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
