2f9ad145da6f76d352665e8c9cbb0bb78d77b203
Muhammad Nur Hakimi Husni <hakimihusni97@gmail.com>
2024-03-18 15:14:00 +0800

    Updated the style guide to follow the Airbnb lint style

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,4 @@
-exports._check = (x,y) => {
-  // First, move the duplicate error checking code here
+exports._check = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
@@ -9,22 +8,22 @@ exports._check = (x,y) => {
 };
 
 exports.add = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x / y;
 };
 
