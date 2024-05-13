83e30a35f8b4a7de5e14d36863a0088db7aa2237
Searchingdays <pachatyabhaskar@gmail.com>
2024-05-13 10:31:28 +0530

    Implement check

--- src/calculator.js
+++ src/calculator.js
@@ -3,6 +3,48 @@ exports._check = () => {
   // First, move the duplicate error checking code here
   // Then, invoke this function inside each of the others
   // HINT: you can invoke this function with exports._check()
+  describe('_check', () => {
+    beforeEach(() => {
+      sinon.spy(calculator, '_check');
+    });
+  
+    afterEach(() => {
+      calculator._check.restore();
+    });
+  
+    it('should throw a TypeError if arguments are not numbers', (exports._check) => {
+      expect(() => calculator._check(40, '2')).to.throw(TypeError);
+      expect(() => calculator._check(40, [])).to.throw(TypeError);
+      expect(() => calculator._check(40, {})).to.throw(TypeError);
+      expect(() => calculator._check('40', 2)).to.throw(TypeError);
+      expect(() => calculator._check([], 2)).to.throw(TypeError);
+      expect(() => calculator._check({}, 2)).to.throw(TypeError);
+    });
+  
+    it('should be called once in "add"', (exports._check) => {
+      calculator.add(40, 2);
+      expect(calculator._check).to.have.been.calledOnce;
+      expect(calculator._check).to.have.been.calledWith(40, 2);
+    });
+  
+    it('should be called once in "subtract"', (exports._check) => {
+      calculator.subtract(44, 2);
+      expect(calculator._check).to.have.been.calledOnce;
+      expect(calculator._check).to.have.been.calledWith(44, 2);
+    });
+  
+    it('should be called once in "multiply"', (exports._check) => {
+      calculator.multiply(6, 7);
+      expect(calculator._check).to.have.been.calledOnce;
+      expect(calculator._check).to.have.been.calledWith(6, 7);
+    });
+  
+    it('should be called once in "divide"', (exports._check) => {
+      calculator.divide(84, 2);
+      expect(calculator._check).to.have.been.calledOnce;
+      expect(calculator._check).to.have.been.calledWith(84, 2);
+    });
+  });
 };
 
 exports.add = (x, y) => {
