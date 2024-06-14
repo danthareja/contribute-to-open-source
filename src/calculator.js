52463e8d0c59f2d8188f9d75dda9f893db4c7e7d
javanpang <javanpang@hotmail.com>
2024-06-14 15:23:28 +0100

    Fix code style

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,9 @@
 exports._check = (a, b) => {
   if (typeof a !== 'number') {
-    throw new TypeError(`${a} is not a number`)
+    throw new TypeError(`${a} is not a number`);
   }
   if (typeof b !== 'number') {
-    throw new TypeError(`${b} is not a number`)
+    throw new TypeError(`${b} is not a number`);
   }
 };
 
