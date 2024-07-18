2312bb3fac8eadd86de738fb198d817ef7d1cc34
yotors <yotorcoder@gmail.com>
2024-07-18 15:14:19 +0300

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,5 @@
-exports._check = (x,y) => {if (typeof x !== 'number') {
+exports._check = (x, y) => {
+  if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
@@ -7,22 +8,22 @@ exports._check = (x,y) => {if (typeof x !== 'number') {
 };
 
 exports.add = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x / y;
 };
 
