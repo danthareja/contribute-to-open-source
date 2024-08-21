6eb5b1a84e396f061e0f64dffecbce54e023f188
KevinJudith <39331879+KevinJudith@users.noreply.github.com>
2024-08-21 16:25:20 +0800

    add style guide

--- src/calculator.js
+++ src/calculator.js
@@ -1,30 +1,30 @@
-function _check(x,y){
-  if(typeof x !== 'number'){
+function _check(x, y){
+  if (typeof x !== 'number'){
     throw new TypeError(`${x} is not a number`);
   }
-  if(typeof y !== 'number'){
+  if (typeof y !== 'number'){
     throw new TypeError(`${y} is not a number`);
   }
-};
+}
 exports._check = _check;
 
 exports.add = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x,y);
+  exports._check(x, y);
   return x / y;
 };
 
