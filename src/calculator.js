e997bcf891f39e5525dbed07cd6b0c5858f2975b
Akhmadillo Mamirov <105142060+akhmadmamirov@users.noreply.github.com>
2024-09-20 14:40:10 -0400

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -5,22 +5,22 @@ exports._check = (num1, num2) => {
 };
 
 exports.add = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x, y)
+  exports._check(x, y);
   return x / y;
 };
 
