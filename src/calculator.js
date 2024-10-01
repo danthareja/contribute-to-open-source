adfd032d695c4fc4405c0e50442c04bf3ec1e9f6
Anshul2122 <139487785+Anshul2122@users.noreply.github.com>
2024-10-01 20:34:41 +0530

    added power and square root of number

--- src/calculator.js
+++ src/calculator.js
@@ -44,5 +44,24 @@ exports.divide = (x, y) => {
   }
   return x / y;
 };
+exports.power = (x,y)=>{
+  if(typeOf x!=== 'number'){
+    throw new TypeError(`${x} is not a number`);
+  }
+  if(typeOf y!=== 'number'){
+    throw new TypeError(`${y} is not a number`);
+  }
+  return Math.pow(x,y);
+}
+
+exports.root = (x)=>{
+  if(typeOf x!=== 'number'){
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (x < 0) {
+    throw new RangeError(`${x} is not a valid input for square root`);
+  }
+  return Math.sqrt(x);
+}
 
 module.exports = exports;
