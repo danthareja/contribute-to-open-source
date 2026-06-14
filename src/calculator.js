e7efdf4209e83eeb88095909d5dd81adc2ef1e9e
Admin User <admin@example.com>
2026-06-14 15:22:00 +0530

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,5 @@
-exports._check = (x, y) => {
+// Central validator
+const check = (x, y) => {
   if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
@@ -6,24 +7,22 @@ exports._check = (x, y) => {
     throw new TypeError(`${y} is not a number`);
   }
 };
-exports.add = (x, y) => {
-  exports._check(x, y);
-  return x + y;
+
+// Wrapper to avoid repeating validation in every function
+const withCheck = (fn) => (x, y) => {
+  check(x, y);
+  return fn(x, y);
 };
 
-exports.subtract = (x, y) => {
-  exports._check(x, y);
-  return x - y;
-};
+exports.add = withCheck((x, y) => x + y);
 
-exports.multiply = (x, y) => {
-  exports._check(x, y);
-  return x * y;
-};
+exports.subtract = withCheck((x, y) => x - y);
 
-exports.divide = (x, y) => {
-  exports._check(x, y);
-  return x / y;
-};
+exports.multiply = withCheck((x, y) => x * y);
+
+exports.divide = withCheck((x, y) => x / y);
+
+// optional export if tests require it
+exports._check = check;
 
 module.exports = exports;
