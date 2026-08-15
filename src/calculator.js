45683c55236a564366867c5207b1f61220085f01
Aman <amanguptawork2726@gmail.com>
2026-08-15 20:59:13 +0530

    New Contri

--- src/calculator.js
+++ src/calculator.js
@@ -1,48 +1,54 @@
-exports._check = () => {
-  // DRY up the codebase with this function
-  // First, move the duplicate error checking code here
-  // Then, invoke this function inside each of the others
-  // HINT: you can invoke this function with exports._check()
+exports._check = (x, y) => {
+  if (typeof x !== 'number' || Number.isNaN(x)) {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (y !== undefined && (typeof y !== 'number' || Number.isNaN(y))) {
+    throw new TypeError(`${y} is not a number`);
+  }
 };
 
 exports.add = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  exports._check(x, y);
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  exports._check(x, y);
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
-  }
+  exports._check(x, y);
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  if (typeof x !== 'number') {
-    throw new TypeError(`${x} is not a number`);
-  }
-  if (typeof y !== 'number') {
-    throw new TypeError(`${y} is not a number`);
+  exports._check(x, y);
+  if (y === 0) {
+    throw new RangeError('Division by zero');
   }
   return x / y;
+};
+
+exports.modulo = (x, y) => {
+  exports._check(x, y);
+  if (y === 0) {
+    throw new RangeError('Modulo by zero');
+  }
+  return x % y;
+};
+
+exports.power = (x, y) => {
+  exports._check(x, y);
+  return x ** y;
+};
+
+exports.sqrt = (x) => {
+  exports._check(x);
+  if (x < 0) {
+    throw new RangeError('Cannot calculate square root of a negative number');
+  }
+  return Math.sqrt(x);
 };
 
 module.exports = exports;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
+describe('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
@@ -17,6 +17,11 @@ describe.skip('_check', () => {
     expect(() => calculator._check('40', 2)).to.throw(TypeError);
     expect(() => calculator._check([], 2)).to.throw(TypeError);
     expect(() => calculator._check({}, 2)).to.throw(TypeError);
+  });
+
+  it('should throw a TypeError if argument is NaN', () => {
+    expect(() => calculator._check(NaN, 2)).to.throw(TypeError);
+    expect(() => calculator._check(40, NaN)).to.throw(TypeError);
   });
 
   it('should be called once in "add"', () => {
@@ -41,6 +46,24 @@ describe.skip('_check', () => {
     calculator.divide(84, 2);
     expect(calculator._check).to.have.been.calledOnce;
     expect(calculator._check).to.have.been.calledWith(84, 2);
+  });
+
+  it('should be called once in "modulo"', () => {
+    calculator.modulo(10, 3);
+    expect(calculator._check).to.have.been.calledOnce;
+    expect(calculator._check).to.have.been.calledWith(10, 3);
+  });
+
+  it('should be called once in "power"', () => {
+    calculator.power(2, 3);
+    expect(calculator._check).to.have.been.calledOnce;
+    expect(calculator._check).to.have.been.calledWith(2, 3);
+  });
+
+  it('should be called once in "sqrt"', () => {
+    calculator.sqrt(16);
+    expect(calculator._check).to.have.been.calledOnce;
+    expect(calculator._check).to.have.been.calledWith(16);
   });
 });
 
@@ -123,6 +146,10 @@ describe('divide', () => {
     expect(() => calculator.divide({}, 2)).to.throw(TypeError);
   });
 
+  it('should throw a RangeError when dividing by zero', () => {
+    expect(() => calculator.divide(40, 0)).to.throw(RangeError, 'Division by zero');
+  });
+
   it('should divide two positive numbers', () => {
     expect(calculator.divide(84, 2)).to.equal(42);
   });
@@ -133,5 +160,47 @@ describe('divide', () => {
 
   it('should divide one positive number and one negative number', () => {
     expect(calculator.divide(84, -2)).to.equal(-42);
+  });
+});
+
+describe('modulo', () => {
+  it('should throw a TypeError if arguments are not numbers', () => {
+    expect(() => calculator.modulo(10, '3')).to.throw(TypeError);
+  });
+
+  it('should throw a RangeError when modulo by zero', () => {
+    expect(() => calculator.modulo(10, 0)).to.throw(RangeError, 'Modulo by zero');
+  });
+
+  it('should return the correct remainder', () => {
+    expect(calculator.modulo(10, 3)).to.equal(1);
+    expect(calculator.modulo(14, 4)).to.equal(2);
+  });
+});
+
+describe('power', () => {
+  it('should throw a TypeError if arguments are not numbers', () => {
+    expect(() => calculator.power(2, '3')).to.throw(TypeError);
+  });
+
+  it('should calculate exponentiation correctly', () => {
+    expect(calculator.power(2, 3)).to.equal(8);
+    expect(calculator.power(5, 0)).to.equal(1);
+    expect(calculator.power(4, 0.5)).to.equal(2);
+  });
+});
+
+describe('sqrt', () => {
+  it('should throw a TypeError if argument is not a number', () => {
+    expect(() => calculator.sqrt('16')).to.throw(TypeError);
+  });
+
+  it('should throw a RangeError for negative inputs', () => {
+    expect(() => calculator.sqrt(-9)).to.throw(RangeError, 'Cannot calculate square root of a negative number');
+  });
+
+  it('should return the square root of a valid number', () => {
+    expect(calculator.sqrt(16)).to.equal(4);
+    expect(calculator.sqrt(0)).to.equal(0);
   });
 });
