a537ee1171e24332e6f1e0d376b16f14e5901714
khaled sbai <Khaledsbai4066@gmail.com>
2026-09-05 22:12:38 +0000

    update file

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
+describe.only('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
